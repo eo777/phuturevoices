ed2520d88881a5987381bf55bcd301cb0a179d39
Eric Oetker <eoetker@gmail.com>
2019-03-03 16:07:18 -0600

    Updates

---
+++ precache-manifest.de227ed7577051a00138aeddccd758b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b441626a1e17387e6925",
+    "url": "/phuturevoices/static/css/main.90efe449.chunk.css"
+  },
+  {
+    "revision": "b441626a1e17387e6925",
+    "url": "/phuturevoices/static/js/main.b441626a.chunk.js"
+  },
+  {
+    "revision": "462c20d80492cf18cc05",
+    "url": "/phuturevoices/static/js/runtime~main.462c20d8.js"
+  },
+  {
+    "revision": "9f474d4141ebdd89bcd3",
+    "url": "/phuturevoices/static/js/2.9f474d41.chunk.js"
+  },
+  {
+    "revision": "b8ccc1c7c78cbcefefac3a97253f5a27",
+    "url": "/phuturevoices/static/media/test1.b8ccc1c7.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/phuturevoices/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3f36f7cbe202b699f31b98688ca36f1",
+    "url": "/phuturevoices/index.html"
+  }
+];
