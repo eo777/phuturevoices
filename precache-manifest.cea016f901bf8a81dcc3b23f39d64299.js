a41b8b11c6764541cb48022f850ec15e0215b79d
Eric Oetker <eoetker@gmail.com>
2019-03-08 19:38:05 -0600

    Updates

--- precache-manifest.cea016f901bf8a81dcc3b23f39d64299.js
+++ precache-manifest.cea016f901bf8a81dcc3b23f39d64299.js
@@ -1,23 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "f3360644591c5276c65c",
+    "revision": "2926763a68d5b1e40c93",
     "url": "/phuturevoices/static/css/main.ef259101.chunk.css"
   },
   {
-    "revision": "f3360644591c5276c65c",
-    "url": "/phuturevoices/static/js/main.ff4387bb.chunk.js"
+    "revision": "2926763a68d5b1e40c93",
+    "url": "/phuturevoices/static/js/main.1894ae4d.chunk.js"
   },
   {
     "revision": "462c20d80492cf18cc05",
     "url": "/phuturevoices/static/js/runtime~main.4a63a6ff.js"
   },
   {
-    "revision": "c2d81ddcc2f7afd0c097",
-    "url": "/phuturevoices/static/js/2.ce338149.chunk.js"
-  },
-  {
-    "revision": "8f2a9ec4b8f29eb539fdb3a75bc28703",
-    "url": "/phuturevoices/static/media/image.8f2a9ec4.jpg"
+    "revision": "f75555a29c0b69f786de",
+    "url": "/phuturevoices/static/js/2.121086b1.chunk.js"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
@@ -28,7 +24,11 @@ self.__precacheManifest = [
     "url": "/phuturevoices/static/media/test1.b8ccc1c7.jpg"
   },
   {
-    "revision": "06df3db57f157d00e2972ef63b3e3e44",
+    "revision": "8f2a9ec4b8f29eb539fdb3a75bc28703",
+    "url": "/phuturevoices/static/media/image.8f2a9ec4.jpg"
+  },
+  {
+    "revision": "ba8c6f152cb86a8112ef49fca65a3f6d",
     "url": "/phuturevoices/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/phuturevoices/precache-manifest.207a54f4927977551b3a2a868b737c3e.js"
+  "/phuturevoices/precache-manifest.cea016f901bf8a81dcc3b23f39d64299.js"
 );
 
 workbox.clientsClaim();
