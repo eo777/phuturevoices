7172880ac88bed66b073957d77c297c5130dc4fe
Eric Oetker <eoetker@gmail.com>
2019-03-08 20:20:37 -0600

    Updates

---
+++ precache-manifest.2bdeff38347caa74b94da3361f69854f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d3cc341b9e1ef0f96d8",
+    "url": "/static/css/main.ef259101.chunk.css"
+  },
+  {
+    "revision": "6d3cc341b9e1ef0f96d8",
+    "url": "/static/js/main.97f59e5b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f75555a29c0b69f786de",
+    "url": "/static/js/2.121086b1.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b8ccc1c7c78cbcefefac3a97253f5a27",
+    "url": "/static/media/test1.b8ccc1c7.jpg"
+  },
+  {
+    "revision": "8f2a9ec4b8f29eb539fdb3a75bc28703",
+    "url": "/static/media/image.8f2a9ec4.jpg"
+  },
+  {
+    "revision": "578f3c98c5d19a4d937333d965ff7cae",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/phuturevoices/precache-manifest.cea016f901bf8a81dcc3b23f39d64299.js"
+  "/precache-manifest.2bdeff38347caa74b94da3361f69854f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/phuturevoices/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
